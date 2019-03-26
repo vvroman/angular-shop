45e97292c89bcd20aed74af83202b69b6bfaf952
Roman Vovk <vovkroman@gmail.com>
2019-03-27 00:11:10 +0200

    created ProductService

---
+++ src/app/products/services/products.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "../models/product.model";
+
+import { productsMock } from "../mocks/products.mock";
+
+@Injectable({ providedIn: 'root'})
+export class ProductsService {
+  getProducts(): Promise<IProduct[]> {
+    return Promise.resolve(productsMock);
+  }
+}
